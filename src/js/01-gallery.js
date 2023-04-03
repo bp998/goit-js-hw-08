6419ebe50d62079bd1013693759cda6f2e4cf245
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-04-03 13:23:13 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+// DATA
+
+galleryItems.forEach(n => {
+  const image = `
+  <a class="gallery__item" href="${n.original}">
+    <img
+      class="gallery__image"
+      src="${n.preview}"
+      alt="${n.description}"
+      data-source="${n.original}"
+  /></a>
+`;
+  gallery.insertAdjacentHTML('beforeend', image);
+});
+
+var lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
